f5a8baa2c5a3ef85c54c03c77e59a2e5e2138bd4
kevalvegeto <kevalgohil@hotmail.com>
2018-09-20 11:24:49 +0530

    gulp update for text-intelligence plugin

--- text-intelligence/gulpfile.js
+++ text-intelligence/gulpfile.js
@@ -1,18 +1,21 @@
 var gulp = require('gulp'),
   inline = require('gulp-inline'),
   uglify = require('gulp-uglify'),
+  env = require('babel-preset-env'),
   minifyCss = require('gulp-minify-css'),
-  babel = require('gulp-babel')
+  babel = require('gulp-babel');
 
 
-gulp.task('build', function () {
+gulp.task('build',() => {
   return gulp.src('./source/index.html')
     .pipe(inline({
       js: [babel({
-        presets: ['es2015']
+        presets: ['env']
       }), uglify],
       css: [minifyCss],
       disabledTypes: ['svg', 'img']
     }))
     .pipe(gulp.dest('./'));
 });
+
+gulp.task('default', gulp.series('build'))
