e6aa0e71f61188eca5fe3e6bab2762e0adcd2671
Amitkanswal <amit.kanswal@contentstack.com>
2021-07-12 17:25:14 +0530

    feat:added youtube extension [CS-11918]

---
+++ youtube/youtube-extension-popup/README.md
@@ -0,0 +1,17 @@
+[![Contentstack](/youtube/youtube-extension-popup/public/contentstack.png)](https://www.contentstack.com/)
+
+## Steps required to create new build
+
+First the app in vscode or any editor if your preference.
+Then fire up the terminal in root directory of the project and used following command
+
+```
+npm install
+npm run build
+or
+yarn 
+yarn build
+```
+After creating the build open up the stack where we are using this app.
+Goto to the assets section and select add assets.
+Now browse the directories fetch index.html from build folder of this project.
---
+++ youtube/youtube-extension-popup/src/components/loader.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export default class Loader extends React.Component {
+  render() {
+    return (
+      <div className="loader">
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+}
