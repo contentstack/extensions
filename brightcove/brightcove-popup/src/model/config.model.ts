58da291fe3dd0cc885673f1674dbd3d3131574e6
Amitkanswal <amit.kanswal@contentstack.com>
2021-08-31 20:15:40 +0530

    fix:updated version and License [CS-16657]

--- brightcove/brightcove-popup/src/model/config.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ConfigObj {
-    oauthUrl:string;
-    proxyUrl:string;
-    searchUrl:string;
-    redirectUrl:string;
-    brightcoveUrl:string;
-    videocountUrl:string;
-    saveFullResponse:boolean;
-}
--- brightcove/brightcove-popup/src/model/modal.model.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigObj } from "./config.model";
-import { VideoList } from "./videoList.model";
-
-export interface ModelProps {
-    config: ConfigObj;
-    selectedVideos: VideoList[];
-    closeWindow: (videos:VideoList[])=>void;
-
-}
