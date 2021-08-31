6eaa3c4f5c18551067b83a5296a5339dbe3a6e00
Amitkanswal <amit.kanswal@contentstack.com>
2021-08-31 11:27:47 +0530

    fix:vulnerabilities fixes [CS-16657]

--- dashboard-widget-google-analytics/gulpfile.js
+++ dashboard-widget-google-analytics/gulpfile.js
@@ -1,23 +1,33 @@
-const gulp = require('gulp');
-const inline = require('gulp-inline');
-const uglify = require('gulp-uglify');
-const minifyCss = require('gulp-clean-css');
-const babel = require('gulp-babel');
-
-  gulp.task('build', () => {
-    return gulp.src('./src/index.html')
-      .pipe(inline({
-        js: [babel({
-          presets: ['es2015']
-        }), uglify],
+const gulp = require('gulp'),
+  inline = require('gulp-inline'),
+  uglify = require('gulp-uglify'),
+  minifyCss = require('gulp-clean-css'),
+  babel = require('gulp-babel'),
+  concat = require('gulp-concat');
+gulp.task('js', function () {
+  return gulp
+    .src('./src/analytics.js')
+    .pipe(concat('scripts.js'))
+    .pipe(
+      babel({
+        presets: ['@babel/env'],
+      })
+    )
+    .pipe(gulp.dest('./src/dist'));
+});
+gulp.task('inline', function () {
+  return gulp
+    .src('./src/index.html')
+    .pipe(
+      inline({
         css: [minifyCss],
-        disabledTypes: ['svg', 'img']
-      }))
-      .pipe(gulp.dest('./'));
-  });
-
-  gulp.task('watch', function() {
-    gulp.watch('src/*', gulp.series('build'));
-  });
-    
-  gulp.task('default', gulp.series('build'));
+        js: uglify,
+      })
+    )
+    .pipe(gulp.dest('./'));
+});
+gulp.task('watch', function () {
+  gulp.watch(['src/dist/*', 'src/*'], gulp.series('build'));
+});
+gulp.task('build', gulp.series('js', 'inline'));
+gulp.task('default', gulp.series('build'));
