455743e99dc33847a7e028c4d93a011eb8777732
Amitkanswal <amit.kanswal@contentstack.com>
2021-08-31 20:15:40 +0530

    fix:updated version and License [CS-16657]

--- brightcove/brightcove/src/model/windowOpener.model.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import { VideoList } from "./videoList.model";
-
-export interface WindowOpner {
-    children: ReactNode;
-    url:string;
-    videos:VideoList[];
-    bridge:(error: object, res: {message:string;})=> void;
-}
