04c54567b1c789aed9aa1492951a4cc28f454229
Ashwini-Rathod <ashwinirathod.f1@gmail.com>
2022-06-07 11:06:57 +0530

    made reviw changes

--- variable-app/src/common/locale/en-us/index.ts
+++ variable-app/src/common/locale/en-us/index.ts
@@ -2,15 +2,11 @@ const localeTexts = {
   configFields: {
     field1: {
       label: "Content Type UID",
-      // help: "Help text for field 1",
       placeholder: "content_type_uid",
-      // instruction: "Instruction for field 1",
     },
     field2: {
       label: "Field",
-      // help: "Help text for field 2",
       placeholder: "field_uid",
-      // instruction: "Instruction for field 2",
     },
   },
 };
