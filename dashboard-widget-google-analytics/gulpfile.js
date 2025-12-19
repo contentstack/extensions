1b769afe6cf77852afd7463451a198b85b963d4f
Suvish Thoovamalayil <suvish.thoovamalayil@contentstack.com>
2025-12-19 16:30:09 +0530

    Fixed dashboard-widget-google-analytics vulnerabilties

--- dashboard-widget-google-analytics/gulpfile.js
+++ dashboard-widget-google-analytics/gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp'),
-  inline = require('gulp-inline'),
   uglify = require('gulp-uglify'),
   minifyCss = require('gulp-clean-css'),
   babel = require('gulp-babel'),
-  concat = require('gulp-concat');
+  concat = require('gulp-concat'),
+  fs = require('fs'),
+  path = require('path');
+
 gulp.task('js', function () {
   return gulp
     .src('./src/analytics.js')
@@ -13,21 +15,30 @@ gulp.task('js', function () {
         presets: ['@babel/env'],
       })
     )
+    .pipe(uglify())
     .pipe(gulp.dest('./src/dist'));
 });
-gulp.task('inline', function () {
-  return gulp
-    .src('./src/index.html')
-    .pipe(
-      inline({
-        css: [minifyCss],
-        js: uglify,
-      })
-    )
-    .pipe(gulp.dest('./'));
+
+gulp.task('inline', async function () {
+  const { inlineSource } = require('inline-source');
+  const htmlFile = './src/index.html';
+  const outputFile = './index.html';
+  
+  try {
+    const html = await inlineSource(htmlFile, {
+      compress: false,
+      rootpath: path.resolve('./src')
+    });
+    fs.writeFileSync(outputFile, html);
+  } catch (err) {
+    console.error('Error inlining source:', err);
+    throw err;
+  }
 });
+
 gulp.task('watch', function () {
   gulp.watch(['src/dist/*', 'src/*'], gulp.series('build'));
 });
+
 gulp.task('build', gulp.series('js', 'inline'));
 gulp.task('default', gulp.series('build'));
