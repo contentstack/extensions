35f5cf0378e7269e2ddf18f29c3c3f1bc716f7e4
Pritesh Jain <prit.jain86@gmail.com>
2025-12-23 14:15:07 +0530

    fix: resolve npm audit security vulnerabilities
    - Replace deprecated gulp-inline with gulp-inline-source
    - Add npm overrides for nth-check, terser, and svgo
    - Update gulpfile and source HTML for new inline-source API
    - All 7 high-severity vulnerabilities resolved

--- external-api-lookup-template/src/dist/scripts.js
+++ external-api-lookup-template/src/dist/scripts.js
@@ -1,24 +1,20 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 var extensionField;
-
 var Request = /*#__PURE__*/function () {
   function Request(_ref) {
     var configArg1 = _ref.configArg1,
-        configArg2 = _ref.configArg2;
-
+      configArg2 = _ref.configArg2;
     _classCallCheck(this, Request);
-
     this.configArg1 = configArg1;
     this.configArg2 = configArg2;
   }
-
   _createClass(Request, [{
     key: "get",
     value: function get() {
@@ -59,42 +55,36 @@ var Request = /*#__PURE__*/function () {
       });
     }
   }]);
-
   return Request;
 }();
-
 function domChangeListner() {
   $('#resource-select').on('change', function () {
     var fieldValue = $('#resource-select').val();
     return extensionField.field.setData(fieldValue);
   });
 }
-
 function render(response) {
   //  to get previously selected
   var fieldData = extensionField.field.getData();
   response.data.forEach(function (dataSet) {
     var option = $('<option></option>').attr('value', dataSet.id).text("".concat(dataSet.first_name, " ").concat(dataSet.last_name));
-
     if (fieldData === dataSet.id.toString()) {
       option.attr('selected', 'selected');
     }
-
     $('#resource-select').append(option);
-  }); // Step 3:  domChangeListner - Start listner on select field to set data for field
+  });
+  // Step 3:  domChangeListner - Start listner on select field to set data for field
   // on selection change
-
   domChangeListner();
 }
-
 $(document).ready(function () {
   // Step:1 Intializing extension - In this step we try to connect to host
   //  window using postMessage API and get intial data.
+
   ContentstackUIExtension.init().then(function (extension) {
     // current extension object
     extensionField = extension;
     var request = new Request(extension.config); // initialize request object using config
-
     request.get().then(function (response) {
       // Step 2:  Render - Render the data fetched from external service
       render(response);
